a0064c6441ec18f300cde360f32fa556dc54c6f3
史泓 <shih01@servyou.com.cn>
2021-06-04 16:49:26 +0800

    fix(creat): 创建组件的时候为每个组件单独创建 store 实例 而不是让多个组件共用一个实例

--- src/create-component.js
+++ src/create-component.js
@@ -3,8 +3,8 @@ import { createStore } from './create-store'
 
 export function createComponent(component, membrane = {}) {
   component.membrane = membrane
-  const useComponent = createStore(component)
   return function HookComponent(props) {
+    const useComponent = createStore(component)
     const store = useComponent(props)
     return <>{store.view.render()}</>
   }
