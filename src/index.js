4bf7d8cabe2784936bb85e49faf89ffa513f37d7
史泓 <shih01@servyou.com.cn>
2021-06-18 14:09:08 +0800

    fix: not export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { createStore } from './create-store'
 export { AppContext } from './app-context'
 export { createStoreContext } from './create-store-context'
-export { createComponent } from './create-component'
+export { createComponent, enhanceCreateComponent } from './create-component'
