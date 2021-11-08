2ceed60e155d34cf2609206ddbe338e717c926a0
zhangnanning <1027963459@qq.com>
2021-11-08 19:45:15 +0800

    feat(router): add beforeDone

--- src/package/@rdeco/router5/beforMiddleware.js
+++ src/package/@rdeco/router5/beforMiddleware.js
@@ -1,5 +1,15 @@
 import { notify } from '../core'
 
+export const beforeDoneMiddleware = (beforeDone) => {
+  return () => (toState, fromState, done) => {
+    if (beforeDone) {
+      beforeDone(toState, fromState, done)
+    } else {
+      done && done()
+    }
+  }
+}
+
 export const beforMiddleware = () => (toState, fromState, done) => {
   notify('@@router', 'before', { toState, fromState, done }).then((done) => {
     done && done()
---
+++ test/routerBeforeDone.test.js
@@ -0,0 +1,38 @@
+import { beforeDoneMiddleware } from '../src/package/@rdeco/router5/beforMiddleware'
+
+describe('测试 beforeDoneMiddleware', () => {
+  it('intercept done', async () => {
+    let flag = 'before'
+    const fn = beforeDoneMiddleware(() => {
+      flag = 'beforeDone'
+    })
+    fn()(null, null, () => {
+      flag = 'done'
+    })
+
+    expect(flag).toContain('beforeDone')
+  })
+
+  it('intercept and perform done', async () => {
+    let flag = 'before'
+    const fn = beforeDoneMiddleware((toState, fromState, done) => {
+      flag = 'beforeDone'
+      done()
+    })
+    fn()(null, null, () => {
+      flag = 'done'
+    })
+
+    expect(flag).toContain('done')
+  })
+
+  it('not intercept', async () => {
+    let flag = 'before'
+    const fn = beforeDoneMiddleware()
+    fn()(null, null, () => {
+      flag = 'done'
+    })
+
+    expect(flag).toContain('done')
+  })
+})
