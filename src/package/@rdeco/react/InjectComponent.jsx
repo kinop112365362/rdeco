c8ca9c5b33c5c15004407826e180d71c8ac1d525
shihong <shih01@servyou.com.cn>
2021-12-09 14:24:16 +0800

    chore(version): v2.7.21

--- src/package/@rdeco/react/InjectComponent.jsx
+++ src/package/@rdeco/react/InjectComponent.jsx
@@ -4,6 +4,9 @@ import { inject } from '../module'
 
 export function Inject(props) {
   const el = React.createRef()
+  if (!props.deps) {
+    throw new Error('props 上没有声明 deps')
+  }
   const deps = props.deps.map((dep) => {
     return props[dep]
   })
