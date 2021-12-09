1a023b49cc95834fb4ad2260b9ae54684fc3ceeb
shihong <shih01@servyou.com.cn>
2021-12-09 11:28:44 +0800

    chore(version): v2.7.14

--- src/package/@rdeco/react/InjectComponent.jsx
+++ src/package/@rdeco/react/InjectComponent.jsx
@@ -1,23 +1,10 @@
 import React, { useEffect } from 'react'
 import { inject } from '../module'
-import { createComponent } from './createComponent'
 
-export const Inject = createComponent({
-  name: `@rdeco/react-inject-${new Date().getTime()}`,
-  ref: {
-    el: React.createRef(),
-  },
-  controller: {
-    onRender() {
-      inject(this.props.name).render(this.ref.el.current, this.props)
-    },
-  },
-  view: {
-    render() {
-      useEffect(() => {
-        this.controller.onRender()
-      }, [this.props])
-      return <div ref={this.ref.el}></div>
-    },
-  },
-})
+export function Inject(props) {
+  const el = React.createRef()
+  useEffect(() => {
+    inject(this.props.name).render(el.current, props)
+  }, [props])
+  return <div ref={this.ref.el}></div>
+}
