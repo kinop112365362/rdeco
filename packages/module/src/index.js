b70fd04469244805aa0fb056b343e3b9409f1924
shihong <shih01@servyou.com.cn>
2022-02-21 14:16:47 +0800

    fix(inject): 不支持 Proxy 时 inject 静默

--- packages/module/src/index.js
+++ packages/module/src/index.js
@@ -2,21 +2,22 @@ import { combination, invoke } from '@rdeco/core'
 /* eslint-disable no-undef */
 export function inject(moduleName) {
   combination.loader(moduleName)
-  if (!Proxy) {
+  if (window.Proxy === undefined) {
     console.error(
       `当前浏览器不支持 Proxy, 无法使用 inject api, 需要降级为 invoke api`
     )
+  } else {
+    return new Proxy(
+      {},
+      {
+        get: function (target, property) {
+          return new Proxy(function () {}, {
+            apply: function (target, thisArg, argumentsList) {
+              return invoke([moduleName], property, ...argumentsList)
+            },
+          })
+        },
+      }
+    )
   }
-  return new Proxy(
-    {},
-    {
-      get: function (target, property) {
-        return new Proxy(function () {}, {
-          apply: function (target, thisArg, argumentsList) {
-            return invoke([moduleName], property, ...argumentsList)
-          },
-        })
-      },
-    }
-  )
 }
