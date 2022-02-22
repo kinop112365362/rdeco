e14157ca250487117c5f9a3fafe3fc8b144cf2dd
shihong <shih01@servyou.com.cn>
2022-02-22 12:24:21 +0800

    chore(jest): 修改 jest 配置测试 req

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const defaults = {
   collectCoverage: true,
   testURL: 'http://localhost',
   setupFiles: ['./jestSetup.js'],
+  transformIgnorePatterns: ['/node_modules/@afe/browsr-runtime-loader'],
 }
 
 const standardConfig = {
--- test/req.test.js
+++ test/req.test.js
@@ -2,22 +2,21 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { req, createComponent, create } from '@rdeco/web-app-sdk'
-
+create({
+  name: '@hrss-component-hrss-data-model/user-module',
+  exports: {
+    login(userInfo, resolve, reject) {
+      if (userInfo.name === 'admin') {
+        resolve('success')
+      } else {
+        reject('faild')
+      }
+    },
+  },
+})
 const userModule = req('@hrss-component/hrss-data-model/user-module')
 
 test('测试 request API', async () => {
-  create({
-    name: '@hrss-component-hrss-data-model/user-module',
-    exports: {
-      login(userInfo, resolve, reject) {
-        if (userInfo.name === 'admin') {
-          resolve('success')
-        } else {
-          reject('faild')
-        }
-      },
-    },
-  })
   const Test = createComponent({
     name: 'test',
     controller: {
