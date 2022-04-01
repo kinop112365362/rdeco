8d757536612692a7670f6d82061be2921d39287a
shihong <shih01@servyou.com.cn>
2022-04-01 17:20:20 +0800

    refactor(name): 对于组件命名不再强限制，只给出警告

--- packages/core/src/utils/validate.js
+++ packages/core/src/utils/validate.js
@@ -3,7 +3,7 @@ import npmValidate from 'validate-npm-package-name'
 export function validate(name) {
   const { validForNewPackages, warnings } = npmValidate(name)
   if (!validForNewPackages) {
-    throw new Error(warnings)
+    console.warn(warnings)
   }
   return name
 }
