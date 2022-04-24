92c762c08173b2f2a4c32d9ada84deefe73e2367
shihong <shih01@servyou.com.cn>
2022-04-24 17:53:34 +0800

    refactor(setter): callback 放在布局变更之后

--- packages/react/src/reactHooks/useStoreUpdate.js
+++ packages/react/src/reactHooks/useStoreUpdate.js
@@ -1,10 +1,10 @@
-import { useEffect, useReducer, useRef } from 'react'
+import { useLayoutEffect, useReducer, useRef } from 'react'
 import { reducer } from '../reducer'
 
 export function useStoreUpdate(store, nextState, props, contextRef) {
   const [state, dispatch] = useReducer(reducer, nextState)
   const ref = useRef(store.ref).current
-  useEffect(() => {
+  useLayoutEffect(() => {
     store.setterCallbacks.forEach((callback = () => {}) => {
       callback()
     })
