d547bf834c381f7998e55faa8abb118652764638
shihong <shih01@servyou.com.cn>
2022-05-24 14:01:41 +0800

    feat(react): 为 createComponent 添加 single 单例模式

--- packages/react/src/createComponent.js
+++ packages/react/src/createComponent.js
@@ -19,6 +19,10 @@ export function createReqComponent(componentConfig) {
 }
 
 export function createComponent(componentConfig) {
+  // 某些极端场景下，对于 React 组件来说即使存在多个 React 实例，也只能保持一个 rdeco 实例，通过手动指定来确定这种单例模式
+  if (componentConfig.single && combination.components[componentConfig.name]) {
+    return
+  }
   const component = deepmerge({}, componentConfig)
   const baseSymbol = component.name
 
