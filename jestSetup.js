0eae8ee87b785a0e381b79261d66a7a5e59a6ffa
shihong <shih01@servyou.com.cn>
2022-06-20 16:46:26 +0800

    feat(req): 修改 ReqApp 和 installHooks 的处理策略

--- jestSetup.js
+++ jestSetup.js
@@ -1,9 +1,1 @@
 window.requestIdleCallback = jest.fn((x) => x())
-
-class MessageChannel {
-  constructor() {
-    this.port1 = null
-    this.prot2 = null
-  }
-}
-window.MessageChannel = MessageChannel
