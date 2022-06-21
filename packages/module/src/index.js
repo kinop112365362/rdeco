28b150ecb684669a09b9416b09d780e2a8680373
shihong <shih01@servyou.com.cn>
2022-06-21 11:06:14 +0800

    fix(ver): 手动刷新 module 版本

--- packages/module/src/index.js
+++ packages/module/src/index.js
@@ -1,7 +1,7 @@
 import { combination, invoke, mock } from '@rdeco/core'
 import { loadRemoteConfig } from '@afe/browser-runtime-loader'
 /* eslint-disable no-undef */
-
+// 手动刷新版本
 export function getIframeWindow(iframeName) {
   return combination.iframeRef[iframeName].contentWindow.rdeco
 }
