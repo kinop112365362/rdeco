f635dfdf581f8d00fbb931a7e3c931f26b775e63
shihong <shih01@servyou.com.cn>
2022-11-17 10:24:09 +0800

    refactor(create): enhance create api when create complex model with membrane and compose

--- packages/core/src/store/create.js
+++ packages/core/src/store/create.js
@@ -2,10 +2,15 @@
 import { combination } from './combination'
 import { createSubscriptions } from '../subscribe/createSubscriptions'
 import { Store } from './Store'
-import deepmerge from 'deepmerge'
+import { createMembrane } from './createMembrane'
+import { compose } from './compose'
 
-function create(entityRaw) {
-  const entity = deepmerge({}, entityRaw)
+function create(entityRaw, membrane = {}, composeList = []) {
+  let entity
+  if (composeList.length > 0) {
+    entity = createMembrane(compose(entityRaw, composeList), membrane)
+  }
+  entity = createMembrane(entityRaw, membrane)
   const symbol = entity.name
   entity.baseSymbol = symbol
   if (
